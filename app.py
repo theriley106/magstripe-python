cfc934373887db631c6f1532a4b9cc14e6ee31f2
Theriley106 <christopherlambert106@gmail.com>
2019-07-06 15:32:18 -0400

    Updated app to work with web sockets

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+from flask_sockets import Sockets
 from flask import Flask, render_template, request, url_for, redirect, Markup, jsonify, make_response, send_from_directory, session
+import msr
+import msrtool
+import sys
+import json
+
+sys.argv.append("/dev/ttyUSB0")
+dev = msr.msr(sys.argv[-1])
 
 app = Flask(__name__, static_url_path='/static')
+sockets = Sockets(app)
 
 
 @app.route('/', methods=['GET'])
@@ -11,5 +20,16 @@ def index():
 def testPage():
 	return render_template("index1.html")
 
+@sockets.route('/echo')
+def echo_socket(ws):
+	x = [None for i in range(3)]
+	while True:
+		#message = ws.receive()
+		try:
+			ws.send(json.dumps(msrtool.mode_read(dev)))
+		except:
+			pass
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+	app.run()
+	#app.run(host='0.0.0.0', port=8000)
