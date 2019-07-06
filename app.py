8fa555504712879967e70538b5a1567c0d08e473
Theriley106 <christopherlambert106@gmail.com>
2019-07-06 17:11:25 -0400

    Got web sockets to work 100%

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import msr
 import msrtool
 import sys
 import json
+import threading
+import time
 
 sys.argv.append("/dev/ttyUSB0")
 dev = msr.msr(sys.argv[-1])
 
 app = Flask(__name__, static_url_path='/static')
 sockets = Sockets(app)
+
+MESSAGES = []
 
 
 @app.route('/', methods=['GET'])
@@ -20,15 +24,36 @@ def index():
 def testPage():
 	return render_template("index1.html")
 
+
+
 @sockets.route('/echo')
 def echo_socket(ws):
+	def check_new():
+		while True:
+			print("CHECKING")
+			message = ws.receive()
+			if message != MESSAGES[-1]:
+				MESSAGES.append(message)
+
 	x = [None for i in range(3)]
+	read = True
+
+	message = ws.receive()
+	MESSAGES.append(message)
+	threading.Thread(target=check_new).start()
 	while True:
-		#message = ws.receive()
-		try:
-			ws.send(json.dumps(msrtool.mode_read(dev)))
-		except:
-			pass
+		message = MESSAGES[-1]
+		if message == "SET TO READ":
+			try:
+				ws.send(json.dumps(msrtool.mode_read(dev)))
+			except Exception as exp:
+				print(exp)
+				pass
+		else:
+			print(message.split("<>"))
+		time.sleep(.1)
+
+
 
 if __name__ == '__main__':
 	app.run()
